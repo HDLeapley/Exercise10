68060ec09319b7e3e4bcd595af6aad72475b10a9
HudsonDL <hudson.d.leapley@gmail.com>
2019-03-21 13:21:38 -0400

    w/comments

--- src/com/leapley/Week10/Main.java
+++ src/com/leapley/Week10/Main.java
@@ -6,7 +6,17 @@ import java.util.List;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
-class Contact {
+/*
+Hudson Leapley 3/21/2019
+Create a class to store contact information consisting of
+a name and email address. Additionally, create a class to
+store a collection of contacts that implements the Iterable
+interface so that a for-each loop can be used with the the
+collection class. Demonstrate this functionality with a
+for-each loop in the Main.main() method that displays
+contact information.
+ */
+class Contact { //contact class that contains name and email
     private String name;
     private String email;
 
@@ -20,7 +30,7 @@ class Contact {
     }
 }
 
-class ContactCollection implements Iterable<Contact>{
+class ContactCollection implements Iterable<Contact>{ //collection class that implements iterable interface
 
     private List<Contact> collection = new ArrayList();
 
@@ -50,15 +60,15 @@ class ContactCollection implements Iterable<Contact>{
 public class Main {
 
     public static void main(String[] args) {
-	    Contact con1 = new Contact("Hudson","gmail@gmail.com");
+        Contact con1 = new Contact("Hudson","gmail@gmail.com");
         Contact con2 = new Contact("Grubb","yahoo@yahoo.com");
         Contact con3 = new Contact("Kevin","sbc@global.com");
-        ContactCollection col = new ContactCollection();
+        ContactCollection col = new ContactCollection(); //makes collection class
         col.add(con1);
         col.add(con2);
         col.add(con3);
 
-        for(Contact con: col){
+        for(Contact con: col){ //display all contacts in collection class.
             con.displayInfo();
         }
     }
